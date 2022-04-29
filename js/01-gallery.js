21dede96003ff3088944ee4403c92cbabeb60ece
zganz <humanmailer@gmail.com>
2022-04-29 23:08:57 +0300

    add hw7

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,37 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+
+
+let imagesHtml = "";
+
+galleryItems.forEach(function(element){
+  let item = `<div class="gallery__item">
+  <a class="gallery__link" href="${element.original}">
+    <img
+      class="gallery__image"
+      src="${element.preview}"
+      data-source="${element.original}"
+      alt="${element.description}"
+    />
+  </a>
+</div>`;
+imagesHtml += item;
+
+});
+const galleryEl = document.querySelector(".gallery")
+galleryEl.insertAdjacentHTML("beforeend", imagesHtml);
+
+
+
+galleryEl.addEventListener("click", handleClick)
+function handleClick(evt){
+    evt.preventDefault();
+    const url = evt.target.dataset.source;
+    basicLightbox.create(`
+		<img width="1400" height="900" src="${url}">
+	`).show()
+    
+}
+
+
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,28 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+let imagesHtml = "";
+
+galleryItems.forEach(function(element){
+  let item = `
+  <a class="gallery__item" href="${element.original}">
+  <img class="gallery__image" src="${element.preview}" alt="${element.description}" />
+</a>`;
+imagesHtml += item;
+
+});
+const galleryEl = document.querySelector(".gallery")
+galleryEl.insertAdjacentHTML("beforeend", imagesHtml);
+
+
+
+galleryEl.addEventListener("click", handleClick)
+function handleClick(evt){
+    evt.preventDefault();
+    const url = evt.target.dataset.source;
+    
+}
+var lightbox = new SimpleLightbox('.gallery a', {
+    'captionsData': 'alt',
+    'captionDelay': 250
+});
